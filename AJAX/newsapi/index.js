c44f5818716046e8077e8f2151b78e7347fbad3a
alegayndra <alegayndra@gmail.com>
2023-09-03 10:05:23 -0600

    fixed news api

--- AJAX/newsapi/index.js
+++ AJAX/newsapi/index.js
@@ -10,25 +10,47 @@
 */
 
 let apikey = "78adee78cfe344268d476a3eb9981538";
-let url = "";
+let url = "https://newsapi.org/v2/everything?";
+
+function newsArticle(article) {
+    const { title, description, url, urlToImage, publishedAt, content, author } = article;
+    console.log(urlToImage);
+    return `<div >
+        <h1>${title}</h1>
+        <h3>${author}</h3>
+        <p>${description}</p>
+        <p>${publishedAt}</p>
+        <p>${content}</p>
+        <a>${url}</a>
+        ${urlToImage ? `<img src="${urlToImage}" width="400px" height="auto" />` : ""}
+    </div>`;
+}
 
 function watchForm() {
-    $("#newsForm").on("click", (event) => {
+    $("#enviar").on("click", (event) => {
         event.preventDefault();
 
         let searchTerm = $("#searchTerm").val();
 
+        console.log(url);
+        console.log(apikey);
+        console.log(searchTerm);
+
         if (searchTerm) {
             $.ajax({
-                url: url,
+                url: `${url}q=${searchTerm}&apiKey=${apikey}`,
                 method: "GET",
-                data: {
-                    apikey: apikey;
-                }, // info sent to the API
+                // data: {
+                //     apikey: apikey
+                // }, // info sent to the API
                 dataType: "json", // returned type of the response
                 ContentType: "application/json", // type of snet data in the request
                 success: function(responseJSON){
                     console.log(responseJSON);
+                    const articles = responseJSON.articles || [];
+                    articles.forEach(article => {
+                        $(".results").append(newsArticle(article));
+                    })
                 },
                 error: function(err){
                     console.log(err);
